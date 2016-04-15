af8de5b53ebca9954c77ae5cecfb93e1eb9d9e8b
Victor <vicserratos@ciencias.unam.mx>
2016-04-14 21:37:00 -0500

    Archivo de prueba con inserts para la base de datos

---
+++ Recursos/insert.sql
@@ -0,0 +1,6 @@
+﻿insert into estado (estado_estado) values ('Aguascalientes') , ('Baja California'), ('Baja California Sur'), ('Campeche'), ('Ciudad de México'), ('Chiapas'), ('Chihuahua'), ('Coahuila'), ('Colima'), ('Durango'), ('Estado de México'), ('Guanajuato'), ('Guerrero'), ('Hidalgo'), ('Jalisco'), ('Michoacán'), ('Morelos'), ('Nayarit'), ('Nuevo León'), ('Oaxaca'), ('Puebla'), ('Querétaro'), ('Quintana Roo'), ('San Luis Potosí'), ('Sinaloa'), ('Sonora'), ('Tabasco'), ('Tamaulipas'), ('Tlaxcala'), ('Veracruz'), ('Yucatán'), ('Zacatecas');
+INSERT INTO usuario (nombre_usuario, contrasenia_usuario) VALUES ('Pepe','jaja');
+insert into nombre_usuario (nombre, apellido_paterno, apellido_materno) values ('Jose', 'Lopez', 'Lopez');
+insert into datos_usuario (nombre_usuario, email, imagen_usuario, telefono, edad, sexo, id_usuario_fk) values (1, 'pepe@gmail.com', 'escritorioo/jjhajha.jpge', 555555555, 21, 'm', 1);
+insert into direccion_usuario (calle, numero, interior, colonia, ciudad, estado_fk, id_datos_usuario) values ('no te topo', 46, 'Z 100', 'tepis', 'Cuauhtemoc', 5, 1);
+
