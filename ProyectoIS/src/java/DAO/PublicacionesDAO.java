0638565e0cf87e3c9f204abf42ab4d56cc57d5b3
Adolfo M <adolffoo@ciencias.unam.mx>
2016-04-17 15:25:02 -0500

    Caso de uso para Publicaciones

---
+++ ProyectoIS/src/java/DAO/PublicacionesDAO.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import beans.PublicacionesBean;
+import java.util.List;
+import org.hibernate.Transaction;
+import mapeo.GaleriaPublicacion;
+import org.hibernate.Session;
+import util.NewHibernateUtil;
+
+/**
+ *
+ * @author adolfo
+ */
+
+
+public class PublicacionesDAO{
+    
+    private List<GaleriaPublicacion> lista;      
+
+    
+    public List<GaleriaPublicacion> getGaleria() {
+        Session session = NewHibernateUtil.getSessionFactory().openSession();
+        try{
+            lista =  (List<GaleriaPublicacion>)session.createQuery("from galeria_publicacion").list();
+        }catch(RuntimeException e){
+        }finally{
+            session.flush();
+            session.close();
+        }
+        return lista;
+    }
+}
+    
+
