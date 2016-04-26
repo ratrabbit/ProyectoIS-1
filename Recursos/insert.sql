20545b008662e3e179dfec24cfb7c71833819eb2
Victor <vicserratos@ciencias.unam.mx>
2016-04-26 13:40:00 -0500

    Actualización del script para insertar.

--- Recursos/insert.sql
+++ Recursos/insert.sql
@@ -1,6 +1,21 @@
-﻿insert into estado (estado_estado) values ('Aguascalientes') , ('Baja California'), ('Baja California Sur'), ('Campeche'), ('Ciudad de México'), ('Chiapas'), ('Chihuahua'), ('Coahuila'), ('Colima'), ('Durango'), ('Estado de México'), ('Guanajuato'), ('Guerrero'), ('Hidalgo'), ('Jalisco'), ('Michoacán'), ('Morelos'), ('Nayarit'), ('Nuevo León'), ('Oaxaca'), ('Puebla'), ('Querétaro'), ('Quintana Roo'), ('San Luis Potosí'), ('Sinaloa'), ('Sonora'), ('Tabasco'), ('Tamaulipas'), ('Tlaxcala'), ('Veracruz'), ('Yucatán'), ('Zacatecas');
-INSERT INTO usuario (nombre_usuario, contrasenia_usuario) VALUES ('Pepe','jaja');
-insert into nombre_usuario (nombre, apellido_paterno, apellido_materno) values ('Jose', 'Lopez', 'Lopez');
-insert into datos_usuario (nombre_usuario, email, imagen_usuario, telefono, edad, sexo, id_usuario_fk) values (1, 'pepe@gmail.com', 'escritorioo/jjhajha.jpge', 555555555, 21, 'm', 1);
-insert into direccion_usuario (calle, numero, interior, colonia, ciudad, estado_fk, id_datos_usuario) values ('no te topo', 46, 'Z 100', 'tepis', 'Cuauhtemoc', 5, 1);
+﻿INSERT INTO usuario VALUES (1,'Pepee','jaja');
 
+insert into datos_usuario values (1, 'Jose', 'Jose', 'Jose', 'pepe@gmail.com', 'periferico', '52', 'H78','perdida','Cuauhtemoc', 100, '21', '45454545', 19, 'm',1);
+
+insert into estado (estado_estado) values ('Aguascalientes') , ('Baja California'), ('Baja California Sur'), ('Campeche'), ('Ciudad de México'), ('Chiapas'), ('Chihuahua'), ('Coahuila'), ('Colima'), ('Durango'), ('Estado de México'), ('Guanajuato'), ('Guerrero'), ('Hidalgo'), ('Jalisco'), ('Michoacán'), ('Morelos'), ('Nayarit'), ('Nuevo León'), ('Oaxaca'), ('Puebla'), ('Querétaro'), ('Quintana Roo'), ('San Luis Potosí'), ('Sinaloa'), ('Sonora'), ('Tabasco'), ('Tamaulipas'), ('Tlaxcala'), ('Veracruz'), ('Yucatán'), ('Zacatecas');
+
+insert into estado values (100,'Ciudad de México');
+
+insert into publicacion (nombre_producto,descripcion,imagenes,precio_inical,precio_final,precio_actual,id_usuario_publicacion) values ('Bicicleta', 'Bicicleta grande, de velocidades', 'publicaciones/bici.jpg',1400, 600,800,1);
+
+insert into publicacion (nombre_producto,descripcion,imagenes,precio_inical,precio_final,precio_actual,id_usuario_publicacion) values ('Microondas', 'Microondas un poco espacioso y pesado', 'publicaciones/microondas.jpg',700, 500,600,1);
+
+insert into publicacion (nombre_producto,descripcion,imagenes,precio_inical,precio_final,precio_actual,id_usuario_publicacion) values ('Laptop', 'Pequeña computadora portatil, pero costosa', 'publicaciones/lap.jpg',1300, 1000,1200,1);
+
+insert into publicacion (nombre_producto,descripcion,imagenes,precio_inical,precio_final,precio_actual,id_usuario_publicacion) values ('Silla de ruedas', 'Silla de ruedas para adulto, compacta', 'publicaciones/sillar.jpg',600, 500,570,1);
+
+insert into publicacion (nombre_producto,descripcion,imagenes,precio_inical,precio_final,precio_actual,id_usuario_publicacion) values ('Comedor', 'Gran comedor con 6 sillas', 'publicaciones/comedor.jpg',900, 500,700,1);
+
+insert into publicacion (nombre_producto,descripcion,imagenes,precio_inical,precio_final,precio_actual,id_usuario_publicacion) values ('Vajilla', 'Pequeña, pero muy frágil', 'publicaciones/vajilla.png',500, 300,450,1);
+
+insert into publicacion (nombre_producto,descripcion,imagenes,precio_inical,precio_final,precio_actual,id_usuario_publicacion) values ('Dron', 'Pequeño y costoso dron', 'publicaciones/dron.jpg',1200, 1000,1000,1);
